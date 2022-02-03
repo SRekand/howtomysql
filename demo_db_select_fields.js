78f0454cea05ee69db522f2f843ac675f3a4431b
Steven Kristjan Rekand <s.rekand@khk.ee>
2022-02-03 18:35:17 +0200

    SQL päring: SELECT (The Fields Object)

--- demo_db_select_fields.js
+++ demo_db_select_fields.js
@@ -11,6 +11,6 @@ con.connect(function(err) {
     if (err) throw err;
     con.query("SELECT name, address FROM customers", function (err, result, fields) {
         if (err) throw err;
-        console.log(result[2].address);
+        console.log(fields[1].name);
     });
 });
