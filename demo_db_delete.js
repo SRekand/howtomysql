1119d2ab17295c3f140f28a784113b5277523d79
Steven Kristjan Rekand <s.rekand@khk.ee>
2022-02-03 18:49:55 +0200

    SQL päring: DELETE (Delete Record)

--- demo_db_delete.js
+++ demo_db_delete.js
@@ -7,11 +7,11 @@ var con = mysql.createConnection( {
     database: "mydb"
 });
 
-
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM customers ORDER BY name DESC", function (err, result) {
+    var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+    con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log(result);
+        console.log("Number of records deleted: " + result.affectedRows);
     });
 });
